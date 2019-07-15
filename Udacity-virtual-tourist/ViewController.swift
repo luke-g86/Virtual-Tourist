f86ba81b9e5ffd4967e6b82b8cf9c8af66b556c1
Lukasz Gajewski <gajewski.lukasz@gmail.com>
2019-07-15 22:06:42 +0700

    initial commit

---
+++ Udacity-virtual-tourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udacity-virtual-tourist
+//
+//  Created by Lukasz on 15/07/2019.
+//  Copyright © 2019 Lukasz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
